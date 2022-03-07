52597e394790f85a2155bfb21e02ec2f95d13e12
jambottle <jaewon.walks@gmail.com>
2022-03-08 00:00:48 +0900

    feat: Add UserView matching with the param

---
+++ vue-news/src/views/UserView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>name: {{ userInfo.id }}</p>
+    <p>karma: {{ userInfo.karma }}</p>
+    <p>created: {{ userInfo.created }}</p>
+  </div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex';
+const { mapState, mapActions } = createNamespacedHelpers('user');
+
+export default {
+  computed: {
+    ...mapState(['userInfo']),
+  },
+
+  methods: {
+    ...mapActions(['GET_USER_INFO']),
+  },
+
+  created() {
+    const userName = this.$route.params.id;
+    this.GET_USER_INFO(userName);
+  },
+};
+</script>
