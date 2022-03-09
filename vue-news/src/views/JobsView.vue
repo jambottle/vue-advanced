56224f53d1eeb84dfa628dc771ebb21d9529dc02
jambottle <jaewon.walks@gmail.com>
2022-03-09 19:35:18 +0900

    design: Style each item of JobsView's list

--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
-    <p v-for="jobs in jobsList" :key="jobs.id">
-      <a :href="jobs.url">{{ jobs.title }}</a>
-      <small>{{ jobs.time_ago }} by {{ jobs.domain }}</small>
-    </p>
+    <ul id="jobs-list">
+      <li id="jobs-item" v-for="jobs in jobsList" :key="jobs.id">
+        <div class="jobs-item__points">
+          {{ jobs.points || 0 }}
+        </div>
+        <div>
+          <p class="jobs-item__title">
+            <a :href="jobs.url">{{ jobs.title }}</a>
+          </p>
+          <small class="jobs-item__user">
+            {{ jobs.time_ago }} by
+            <a :href="`https://www.${jobs.domain}`">
+              {{ jobs.domain }}
+            </a>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -25,3 +39,37 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+#jobs-list {
+  margin: 0;
+  padding: 0;
+
+  #jobs-item {
+    display: flex;
+    align-items: center;
+
+    border-bottom: 1px solid #eeeeee;
+    list-style: none;
+
+    .jobs-item__points {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      width: 80px;
+      height: 60px;
+      color: #42b883;
+    }
+
+    .jobs-item__title {
+      margin: 0;
+    }
+
+    .jobs-item__user,
+    .jobs-item__user a {
+      color: #828282;
+    }
+  }
+}
+</style>
