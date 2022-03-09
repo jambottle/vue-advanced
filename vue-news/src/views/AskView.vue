829c3fb6f1fa458184cc97412d4031bcb5f54a1e
jambottle <jaewon.walks@gmail.com>
2022-03-09 19:30:06 +0900

    design: Style each item of AskView's list

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <p v-for="ask in askList" :key="ask.id">
-      <router-link :to="`/item/${ask.id}`">{{ ask.title }}</router-link>
-      <small>
-        {{ ask.time_ago }} by
-        <router-link :to="`/user/${ask.user}`">{{ ask.user }}</router-link>
-      </small>
-    </p>
+    <ul id="ask-list">
+      <li id="ask-item" v-for="ask in askList" :key="ask.id">
+        <div class="ask-item__points">
+          {{ ask.points }}
+        </div>
+        <div>
+          <p class="ask-item__title">
+            <router-link :to="`/item/${ask.id}`">{{ ask.title }}</router-link>
+          </p>
+          <small class="ask-item__user">
+            {{ ask.time_ago }} by
+            <router-link :to="`/user/${ask.user}`">
+              {{ ask.user }}
+            </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -28,3 +39,37 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+#ask-list {
+  margin: 0;
+  padding: 0;
+
+  #ask-item {
+    display: flex;
+    align-items: center;
+
+    border-bottom: 1px solid #eeeeee;
+    list-style: none;
+
+    .ask-item__points {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      width: 80px;
+      height: 60px;
+      color: #42b883;
+    }
+
+    .ask-item__title {
+      margin: 0;
+    }
+
+    .ask-item__user,
+    .ask-item__user a {
+      color: #828282;
+    }
+  }
+}
+</style>
