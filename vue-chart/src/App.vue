c04b9baa988595577ca7269d3ad3658975eedea8
jambottle <jaewon.walks@gmail.com>
2022-03-30 23:05:19 +0900

    Create a new project with Vue CLI

---
+++ vue-chart/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>hello</div>
+</template>
+
+<style>
+#app {
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
