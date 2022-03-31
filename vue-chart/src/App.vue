d30b3a4cf6ef2b3f71ba43f58ea37b9088c007af
jambottle <jaewon.walks@gmail.com>
2022-03-31 21:55:36 +0900

    feat: Add LineChart component into App.vue

--- vue-chart/src/App.vue
+++ vue-chart/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <h1>Chart.js</h1>
   <BarChart />
+  <LineChart />
 </template>
 
 <script>
 import BarChart from '@/components/BarChart.vue';
+import LineChart from '@/components/LineChart.vue';
 
 export default {
   components: {
     BarChart,
+    LineChart,
   },
 };
 </script>
---
+++ vue-chart/src/components/LineChart.vue
@@ -0,0 +1,30 @@
+<template>
+  <canvas id="lineChart" width="400" height="400" />
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
+export default {
+  mounted() {
+    const ctx = document.getElementById('lineChart').getContext('2d');
+    // eslint-disable-next-line no-unused-vars
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            data: [0, 10, 5, 2, 20, 30, 45],
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+          },
+        ],
+      },
+      options: {},
+    });
+  },
+};
+</script>
